c47e7ac991f4bab6bd2dd81af745b31f70900efc
hasajacyszatan <kamilpl05@outlook.com>
2026-04-24 10:14:05 +0200

    dodanie statica

--- mysite/settings.py
+++ mysite/settings.py
@@ -113,5 +113,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 STATIC_URL = 'static/'
