383206fd2042ced497ada520aa9c62016c5c476a
hasajacyszatan <kamilpl05@outlook.com>
2026-05-24 18:13:34 +0200

    remove app users because it's unnecessary

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'posty',
-    'users',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
